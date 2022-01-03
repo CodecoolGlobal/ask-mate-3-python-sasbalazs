450837fb811dc25b9e2293c311bdbb5834682494
Szeplaki <szeplaki.eva1@gmail.com>
2022-01-03 14:29:04 +0100

    Merge

--- server.py
+++ server.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import connection
 
 app = Flask(__name__)
 
 
+
+
+@app.route("/question/<question_id>")
+def question(question_id):
+    return render_template('question.html')
+
+@app.route("/list")
 @app.route("/")
-def hello():
-    return "Hello World!"
+def list_page():
+    questions = connection.import_data("sample_data/question.csv")
+    answers = connection.import_data("sample_data/answer.csv")
+    return render_template('list.html', questions=questions, answers=answers)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
