e4ae101a456e81ceac807fac38dfeee742f84833
Sas <sasbalazs05@gmail.com>
2022-01-04 14:36:08 +0100

    Added sort function

--- server.py
+++ server.py
@@ -22,13 +22,39 @@ def question(question_id):
                 if question_id == answer['question_id']:
                     answers_to_render = answer
 
-    return render_template('question.html', questions=questions, answers=answers, question_to_render=question_to_render, answers_to_render=answers_to_render)
+    return render_template('question.html', questions=questions, answers=answers, question_to_render=question_to_render,
+                           answers_to_render=answers_to_render)
 
-@app.route("/list")
+
+@app.route("/list", methods=['GET', 'POST'])
 @app.route("/")
 def list_page():
     questions = connection.import_data("sample_data/question.csv")
     answers = connection.import_data("sample_data/answer.csv")
+    if request.method == 'POST':
+        order_by = request.form.get('order_by')
+        order_direction = request.form.get('order_direction')
+        print(order_by)
+        if order_by == 'Title' and order_direction == 'ascending':
+            questions = sorted(questions, key=lambda questions: questions['title'])
+        if order_by == 'Title' and order_direction == 'descending':
+            questions = sorted(questions, key=lambda questions: questions['title'], reverse=True)
+        if order_by == 'View number' and order_direction == 'ascending':
+            questions = sorted(questions, key=lambda questions: int(questions['view_number']))
+        if order_by == 'View number' and order_direction == 'descending':
+            questions = sorted(questions, key=lambda questions: int(questions['view_number']), reverse=True)
+        if order_by == 'Submission time' and order_direction == 'ascending':
+            questions = sorted(questions, key=lambda questions: int(questions['submission_time']))
+        if order_by == 'Submission time' and order_direction == 'descending':
+            questions = sorted(questions, key=lambda questions: int(questions['submission_time']), reverse=True)
+        if order_by == 'Message' and order_direction == 'ascending':
+            questions = sorted(questions, key=lambda questions: len(questions['message']))
+        if order_by == 'Message' and order_direction == 'descending':
+            questions = sorted(questions, key=lambda questions: len(questions['message']), reverse=True)
+        if order_by == 'Number of votes' and order_direction == 'ascending':
+            questions = sorted(questions, key=lambda questions: int(questions['vote_number']))
+        if order_by == 'Number of votes' and order_direction == 'descending':
+            questions = sorted(questions, key=lambda questions: int(questions['vote_number']), reverse=True)
     return render_template('list.html', questions=questions, answers=answers)
 
 
