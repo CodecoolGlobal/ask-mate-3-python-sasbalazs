70f55f953c578be2f4254e401fc430cecad15ee8
Szeplaki <szeplaki.eva1@gmail.com>
2022-01-04 23:08:28 +0100

    Create functionality to ask a question

--- connection.py
+++ connection.py
@@ -4,21 +4,35 @@ DATA_HEADER_QUESTION = ["id","submission_time","view_number","vote_number","titl
 DATA_HEADER_ANSWER = ["id","submission_time","vote_number","question_id","message","image"]
 
 
+def import_data(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+    return data
+
 
 def export_data(data, filename):
+    if filename == 'sample_data/answer.csv':
+        data_header = DATA_HEADER_ANSWER
+    else:
+        data_header = DATA_HEADER_QUESTION
     with open(filename, 'w', encoding='utf-8', newline="") as file:
-        fieldnames=DATA_HEADER
+        fieldnames=data_header
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         for line in data:
             writer.writerow(line)
 
 
-def import_data(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        data = [row for row in reader]
-    return data
+
+
+
+
+
+
+
+
+
 
 
 
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import connection
+import data_manager
 
 app = Flask(__name__)
 
@@ -7,6 +8,31 @@ app = Flask(__name__)
 @app.route("/add-question")
 def add_question():
     return render_template('add-question.html')
+
+@app.route('/display_question', methods=['POST'])
+def display_question():
+    new_question = {}
+    counter = len(connection.import_data('sample_data/question.csv'))+1
+    if request.method == 'POST':
+        id = counter
+        new_question['id'] = str(id)
+        submission_time = data_manager.get_unixtime()
+        new_question['submission_time'] = submission_time
+        new_question['view_number'] = 0
+        new_question['vote_number'] = 0
+        title = request.form['title']
+        new_question['title'] = title
+        message = request.form['message']
+        new_question['message'] = message
+        image = request.form['image']
+        if image:
+            new_question['image'] = image
+        new_question['image'] = None
+
+        questions = connection.import_data('sample_data/question.csv')
+        questions.append(new_question)
+        connection.export_data(questions, 'sample_data/question.csv')
+        return render_template('/display_added_question.html', new_question=new_question)
 
 
 @app.route("/question/<question_id>")
