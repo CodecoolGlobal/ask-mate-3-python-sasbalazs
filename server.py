fc504caae09746da73a95db1dc2f79fadaafb217
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-01-05 12:08:33 +0100

    add vote for answers

--- server.py
+++ server.py
@@ -5,6 +5,34 @@ import data_manager
 app = Flask(__name__)
 
 
+@app.route("/answer/<answer_id>/vote_up", methods=["GET"])
+def vote_up(answer_id):
+    if request.method == 'GET':
+        answers = connection.import_data("sample_data/answer.csv")
+        for answer in answers:
+            if answer["id"] == answer_id:
+                vote_up = int(answer["vote_number"])
+                vote_up += 1
+                answer["vote_number"] = vote_up
+                question_id = answer["question_id"]
+        connection.export_data(answers, 'sample_data/answer.csv')
+        return redirect(url_for("question", question_id=question_id))
+
+
+@app.route("/answer/<answer_id>/vote_down", methods=["GET"])
+def vote_down(answer_id):
+    if request.method == 'GET':
+        answers = connection.import_data("sample_data/answer.csv")
+        for answer in answers:
+            if answer["id"] == answer_id:
+                vote_down = int(answer["vote_number"])
+                vote_down -= 1
+                answer["vote_number"] = vote_down
+                question_id = answer["question_id"]
+        connection.export_data(answers, 'sample_data/answer.csv')
+        return redirect(url_for("question", question_id=question_id))
+
+
 @app.route("/question/<question_id>/new-answer", methods= ['GET', 'POST'])
 def post_new_answer(question_id):
     if request.method == 'GET':
